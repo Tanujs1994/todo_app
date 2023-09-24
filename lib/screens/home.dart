86787935e7ff9c72206798c106c9a29efaf39aae
Tanuj sharma <tanujsharma482@gmail.com>
2023-09-24 20:04:16 +0530

    get all tasks

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/add_task.dart';
+
+class TaskModel{
+  String id;
+  String description;
+  String title;
+
+  TaskModel({required this.id, required this.description, required this.title});
+}
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,16 +19,64 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<TaskModel> myAllTodos = [];
+
+  String uid = '';
+  @override
+  void initState() {
+    getuid();
+    super.initState();
+    getDataFromFirebase();
+  }
+
+  getDataFromFirebase(){
+    final db = FirebaseFirestore.instance;
+    db.collection("tasks").doc(uid).collection("my tasks").get().then(
+  (querySnapshot) {
+    print("Successfully completed");
+    List<TaskModel> fromServer = [];
+    for (var docSnapshot in querySnapshot.docs) {
+      print('${docSnapshot.id} => ${docSnapshot.data()}');
+      fromServer.add(TaskModel(id: docSnapshot.id, description: docSnapshot.get('description'), title: docSnapshot.get('title')));
+    }
+    setState(() {
+      myAllTodos = fromServer;
+    });
+  },
+  onError: (e) => print("Error completing: $e"),
+);
+
+  }
+
+  getuid() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+    setState(() {
+      uid = user!.uid;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('TODO'),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.red,
+      body: ListView.builder(
+        itemCount: myAllTodos.length,
+        itemBuilder: (ctx, indx){
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Column(
+              children: [
+                Text(myAllTodos[indx].id),
+                Text(myAllTodos[indx].title),
+                Text(myAllTodos[indx].description),
+              ],
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
